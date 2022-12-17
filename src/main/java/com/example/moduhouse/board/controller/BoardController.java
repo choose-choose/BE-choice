9077bfcf1c7a94fe23f6bb7c7c9d53995d781169
Sangtriever <dltkdgus1850@gmail.com>
2022-12-18 03:07:58 +0900

    feat : 게시판 기능 upgade, 카테고리별 게시판 조회

--- src/main/java/com/example/moduhouse/board/controller/BoardController.java
+++ src/main/java/com/example/moduhouse/board/controller/BoardController.java
@@ -7,7 +7,6 @@ import com.example.moduhouse.global.MsgResponseDto;
 import com.example.moduhouse.global.exception.SuccessCode;
 import com.example.moduhouse.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -32,22 +31,22 @@ public class BoardController {
     public List<BoardResponseDto> getListBoards(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return boardService.getListBoards(userDetails.getUser());
     }
-//    @GetMapping("/boards/{category}")
-//    public List<BoardResponseDto> getCategoryListBoards(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable String category){
-//        return boardService.getCategoryListBoards(userDetails.getUser(),category);
-//    }
+    @GetMapping("/boards/{category}")
+    public List<BoardResponseDto> getCategoryListBoards(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable String category){
+        return boardService.getCategoryListBoards(userDetails.getUser(),category);
+    }
 
 
 
-//    @GetMapping("/board/{id}")
-//    public BoardResponseDto getBoards(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//        return boardService.getBoard(id, userDetails.getUser());
-//    }
+    @GetMapping("/board/{id}")
+    public BoardResponseDto getBoards(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return boardService.getBoard(id, userDetails.getUser());
+    }
 
-//    @PutMapping("/board/{id}")
-//    public BoardResponseDto updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-//        return boardService.updateBoard(id, requestDto, userDetails.getUser());
-//    }
+    @PutMapping("/board/{id}")
+    public BoardResponseDto updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return boardService.updateBoard(id, requestDto, userDetails.getUser());
+    }
 
     @DeleteMapping("/board/{id}")
     public MsgResponseDto deleteBoard(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
